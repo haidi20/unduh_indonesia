6e5438ffe2a37e9088860b56ccb8f6f04fb98e0b
haidi20 <nhaidii@yahoo.com>
2017-12-08 17:36:02 +0800

    update_landingpage_css

--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="{{asset('css/admin-dashboard.css')}}">
 
     <link rel="stylesheet" href="{{asset('bootstrap/js/bootstrap.js')}}">
-    <link href="{{asset('css/custom.css')}}">
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
 	<script src="{{asset('boostrap/js/jquery')}}"></script>
 
   </head>
